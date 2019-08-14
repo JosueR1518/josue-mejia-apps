53e40d321a648a98806d7b3d9c0ace70f07c524d
Alexander mejia lopez <alexandermejialopez@gmail.com>
2019-08-14 11:33:52 -0600

    agregando seguridad

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: 'AIzaSyD7-wgHxaCWhcHpSZaE0U3HAIQxgtUDZf8',
-    authDomain: 'portfolio-bd5cc.firebaseapp.com',
-    databaseURL: 'https://portfolio-bd5cc.firebaseio.com',
-    projectId: 'portfolio-bd5cc',
-    storageBucket: 'portfolio-bd5cc.appspot.com',
-    messagingSenderId: '650442378053',
-    appId: '1:650442378053:web:4dd36a60729d8c29'
+   
   }
 
 };
