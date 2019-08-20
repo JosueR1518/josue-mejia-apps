6a4cae0fae981fd7bbb76cce72ef8a90ce23ffd3
Alexander mejia lopez <alexandermejialopez@gmail.com>
2019-08-19 22:50:01 -0600

    actualizacion de datos generales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,7 @@ export const environment = {
   production: true,
   firebase : {
 
-    apiKey: "AIzaSyD7-wgHxaCWhcHpSZaE0U3HAIQxgtUDZf8",
-    authDomain: "portfolio-bd5cc.firebaseapp.com",
-    databaseURL: "https://portfolio-bd5cc.firebaseio.com",
-    projectId: "portfolio-bd5cc",
-    storageBucket: "portfolio-bd5cc.appspot.com",
-    messagingSenderId: "650442378053",
-    appId: "1:650442378053:web:4dd36a60729d8c29"
+    
 
   }
 
